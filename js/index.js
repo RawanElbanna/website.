151b19b2533ff3d01313ff3cc5f8533143d491b0
RawanElbanna <99205603+RawanElbanna@users.noreply.github.com>
2022-03-15 16:54:31 +0200

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
+// window.onscroll = function() {myFunction()};
 
+// var navbar = document.getElementById("navbar");
+// var sticky = navbar.offsetTop;
+
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
+// function navbarFixed(){
+//     if ( $('.navbar').length ){ 
+//         $(window).scroll(function() {
+//             var scroll = $(window).scrollTop();   
+//             if (scroll >= nav_offset_top ) {
+//                 $(".navbar").addClass("navbar_fixed");
+//             } else {
+//                 $(".navbar").removeClass("navbar_fixed");
+//             }
+//         });
+//     };
+// };
+// navbarFixed();
